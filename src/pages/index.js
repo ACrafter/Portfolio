40b238c5c9b9a3add7c34d63528a493b05ed1d15
ACrafter <ahmed2002yasser.ay@gmail.com>
2022-08-14 01:12:55 +0200

    Cleaning #1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Landing from '../components/Landing';
-import Layout from '../components/Layout';
+// import Layout from '../components/Layout';
+import Navbar from '../components/Navbar'
+import Sky from '../components/Sky';
+import Footer from '../components/Footer'
 
 export default function App() {
   return (
     <div>
-    <Layout />
+    <Navbar />
+    <Sky />
     <Landing />
+    <Footer />
     </div>
   )
 }
